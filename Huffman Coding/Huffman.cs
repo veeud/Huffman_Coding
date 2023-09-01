b592bdf2384b0215ff8a584cd6cbc7353cef16fb
veeud <62218506+veeud@users.noreply.github.com>
2023-09-01 16:01:56 +0200

    Add files via upload

---
+++ Huffman Coding/Huffman.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_Coding
+{
+    public class HuffmanNode
+    {
+        public char Char { get; set; }
+        public int Frequency { get; set; }
+        public HuffmanNode Left { get; set; }
+        public HuffmanNode Right { get; set; }
+    }
+
+    public class HuffmanTree
+    {
+        public HuffmanNode Root { get; set; }
+
+        public HuffmanTree(List<HuffmanNode> nodes)
+        {
+            while (nodes.Count > 1)
+            {
+                var leftNode = nodes[0];
+                var rightNode = nodes[1];
+
+                HuffmanNode newNode = new HuffmanNode()
+                {
+                    Frequency = leftNode.Frequency + rightNode.Frequency,
+                    Left = leftNode,
+                    Right = rightNode
+                };
+
+                nodes.RemoveRange(0, 2);
+
+                nodes.Add(newNode);
+
+                nodes.Sort((a, b) => a.Frequency.CompareTo(b.Frequency));
+            }
+
+            Root = nodes[0];
+        }
+    }
+}
---
+++ Huffman Coding/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Huffman_Coding
+{
+    internal class Program
+    {
+        //Can be set to any wished value except 0 or 1
+        const char seperator = ' ';
+
+        static Dictionary<char, string> huffmanCodes = new Dictionary<char, string>();
+        static Dictionary<char, int> characters = new Dictionary<char, int>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ProcessAndDisplayResult();
+                Reset();
+            }
+        }
+
+        private static void Reset()
+        {
+            huffmanCodes.Clear();
+            characters.Clear();
+        }
+
+        private static void ProcessAndDisplayResult()
+        {
+            string filePath = GetFilePath();
+
+            string input = File.ReadAllText(filePath);
+            CountCharacters(input);
+
+            HuffmanTree tree = BuildTree();
+            BuildHuffmanCodes(tree.Root);
+            string binary = ConvertToBinary(input);
+            string text = ConvertToText(binary);
+
+            Display(binary, text);
+        }
+
+        private static void Display(string binary, string text)
+        {
+            Console.Write("Press any key to show compressed text (as binary).");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine(binary);
+            Console.WriteLine(Environment.NewLine + "Press any key to decompress to readable text.");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine(text);
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+
+        private static string GetFilePath()
+        {
+            Console.WriteLine("Enter file path:" + Environment.NewLine);
+            string filePath = string.Empty;
+            while (true)
+            {
+                filePath = Console.ReadLine();
+                if (File.Exists(filePath))
+                {
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Not a valid file path, try again:" + Environment.NewLine);
+
+                }
+            }
+
+            return filePath;
+        }
+
+        private static string ConvertToText(string binary)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in binary.Split(seperator))
+            {
+                char character = huffmanCodes.First(x => x.Value == c).Key;
+                sb.Append(character);
+            }
+            return sb.ToString();
+        }
+
+        private static string ConvertToBinary(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in text)
+            {
+                sb.Append(huffmanCodes[c] + seperator);
+            }
+            sb = sb.Remove(sb.Length - 1, 1); 
+            return sb.ToString();
+        }
+
+        private static HuffmanTree BuildTree()
+        {
+            List<HuffmanNode> nodes = new List<HuffmanNode>();
+            for (int i = 0; i < characters.Count; i++)
+            {
+                nodes.Add(new HuffmanNode()
+                {
+                    Char = characters.ElementAt(i).Key,
+                    Frequency = characters.ElementAt(i).Value
+                });
+            }
+            HuffmanTree tree = new HuffmanTree(nodes);
+            return tree;
+        }
+
+        private static void CountCharacters(string text)
+        {
+            foreach (char c in text)
+            {
+                if (characters.ContainsKey(c))
+                    characters[c]++;
+                else
+                    characters.Add(c, 1);
+            }
+            characters = characters.OrderBy(kv => kv.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+
+        static void BuildHuffmanCodes(HuffmanNode node, string bits = "")
+        {
+            if (node != null)
+            {
+                if (node.Left == null && node.Right == null)
+                {
+                    huffmanCodes.Add(node.Char, bits);
+                }
+                BuildHuffmanCodes(node.Left, bits + "0");
+                BuildHuffmanCodes(node.Right, bits + "1");
+            }
+        }
+    }
+}
